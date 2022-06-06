d40eae7d6165e35d542bde9d603c01ad9f0a05ec
Ivan Bortnik <disturbe2011@yandex.ru>
2022-06-06 12:33:41 +0300

    feat: add all block, smooth scrolling

--- scripts.js
+++ scripts.js
@@ -1,10 +1,24 @@
 const menu = document.querySelector('.header__nav')
 const burger = document.querySelector('.header__menu')
+const smoothLinks = document.querySelectorAll('a[href^="#"]');
+
+for (let smoothLink of smoothLinks) {
+  smoothLink.addEventListener('click', function (e) {
+    e.preventDefault();
+    const id = smoothLink.getAttribute('href');
+
+    document.querySelector(id).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    });
+  });
+}
 
 const toggleMenu = () => {
   menu.classList.toggle('active')
   burger.classList.toggle('active')
 }
+
 burger.addEventListener('click', toggleMenu)
 menu.addEventListener('click', (e) => {
   const target = e.target
@@ -12,3 +26,4 @@ menu.addEventListener('click', (e) => {
     toggleMenu()
   }
 })
+
